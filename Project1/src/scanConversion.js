bf57f928758156be51911db2d8cb5b2cd954bc6a
zhouxinan <zhouxinan@protonmail.ch>
2015-10-09 17:42:56 +0800

    Add implementation

--- Project1/src/scanConversion.js
+++ Project1/src/scanConversion.js
@@ -1,55 +1,151 @@
-//该函数在一个canvas上绘制一个点
+//该函数在一个canvas上绘制一条从(x1,y)到(x2,y)的线段。
 //其中cxt是从canvas中获得的一个2d上下文context
-//    x,y分别是该点的横纵坐标
 //    color是表示颜色的整形数组，形如[r,g,b]
-//    color在这里会本转化为表示颜色的字符串，其内容也可以是：
+//    color在这里会转化为表示颜色的字符串，其内容也可以是：
 //        直接用颜色名称:   "red" "green" "blue"
 //        十六进制颜色值:   "#EEEEFF"
 //        rgb分量表示形式:  "rgb(0-255,0-255,0-255)"
-//        rgba分量表示形式:  "rgba(0-255,1-255,1-255,透明度)"
-//由于canvas本身没有绘制单个point的接口，所以我们通过绘制一条短路径替代
-function drawPoint(cxt, x, y, color) {
-	drawLine(cxt, x, y, x + 1, y + 1, color);
-}
-
-// 绘制线段的函数绘制一条从(x1,y1)到(x2,y2)的线段，cxt和color两个参数意义与绘制点的函数相同，
-function drawLine(cxt, x1, y1, x2, y2, color) {
+//        rgba分量表示形式:  "rgba(0-255,0-255,0-255,透明度)"
+function drawLine(cxt, x1, x2, y, color) {
 	// 建立一条新的路径
 	cxt.beginPath();
 	// 设置画笔的颜色
-	cxt.strokeStyle = "rgba(" + color[0] + "," + +color[1] + "," + +color[2]
-			+ "," + +255 + ")";
+	cxt.strokeStyle = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
 	// 这里线宽取1会有色差，但是类似半透明的效果有利于debug，取2效果较好
-	cxt.lineWidth = 1;
+	cxt.lineWidth = 2;
 	// 设置路径起始位置
-	cxt.moveTo(x1, y1);
+	cxt.moveTo(x1, y);
 	// 在路径中添加一个节点
-	cxt.lineTo(x2, y2);
+	cxt.lineTo(x2, y);
 	// 用画笔颜色绘制路径
 	cxt.stroke();
 }
 
+// This function is to draw all vertex circles. The radius of the circles is read from circleRadius, which is defined in the window.onload function.
 function drawCircle() {
-	for (i = 0; i < vertex_pos.length; i++) {
+	for (var i = 0; i < vertex_pos.length; i++) {
 		cxt.beginPath();
-		cxt.arc(vertex_pos[i][0], vertex_pos[i][1], 10, 0, 2 * Math.PI, true);
+		cxt.arc(vertex_pos[i][0], vertex_pos[i][1], circleRadius, 0,
+				2 * Math.PI, true);
 		cxt.closePath();
-		cxt.fillStyle = "rgb(255,0,0)";
+		// The fill color of the circles is red.
+		cxt.fillStyle = "red";
 		cxt.fill();
-		cxt.strokeStyle = "rgb(0,0,0)";
+		// The border color of the circles is black.
+		cxt.strokeStyle = "black";
 		cxt.stroke();
 	}
 }
 
+// This function is to fill a single polygon.
 function fillPolygon(cxt, polygon) {
+	// The vertex array is to store the vertex positions of the polygon.
 	var vertex = [];
-	for (var i = 0; i < 4; i++) {
+	for (var i = 0; i < polygon.length; i++) {
 		vertex[i] = vertex_pos[polygon[i]];
 	}
+	// The polygon's color is the first vertex's color.
 	var color = vertex_color[polygon[0]];
-	for (var i = 0; i < 4; i++) {
-		drawLine(cxt, vertex[i % 4][0], vertex[i % 4][1],
-				vertex[(i + 1) % 4][0], vertex[(i + 1) % 4][1], color);
+	// minY is the minimal y coordinate of the vertexes.
+	// maxY is the maximal y coordinate of the vertexes.
+	var minY = vertex[0][1];
+	var maxY = minY;
+	for (var i = 1; i < polygon.length; i++) {
+		if (vertex[i][1] < minY) {
+			minY = vertex[i][1];
+			continue; // This is an optimization. If vertex[i][1] is less than minY, it could never be larger than maxY.
+		}
+		if (vertex[i][1] > maxY) {
+			maxY = vertex[i][1];
+		}
+	}
+	// Construct activeEdgeTable and newEdgeTable. There are only (maxY - minY + 1) scan lines for the polygon.
+	var scanLineCount = maxY - minY + 1;
+	var activeEdgeTable = new Array(scanLineCount);
+	var newEdgeTable = new Array(scanLineCount);
+	for (var i = 0; i < scanLineCount; i++) {
+		newEdgeTable[i] = [];
+		activeEdgeTable[i] = [];
+	}
+	
+	for (var i = 0; i < polygon.length; i++) {
+		var p1y = vertex[i][1];
+		var p2y = vertex[(i + 1) % polygon.length][1];
+		if (p1y < p2y) {
+			var p1x = vertex[i][0];
+			var p2x = vertex[(i + 1) % polygon.length][0];
+			newEdgeTable[p1y - minY]
+					.push({
+						x : p1x,
+						dx : ((p2x - p1x) / (p2y - p1y)),
+						maxY : p2y
+					});
+		} else if (p1y > p2y) {
+			var p1x = vertex[i][0];
+			var p2x = vertex[(i + 1) % polygon.length][0];
+			newEdgeTable[p2y - minY]
+					.push({
+						x : p2x,
+						dx : ((p2x - p1x) / (p2y - p1y)),
+						maxY : p1y
+					});
+		}
+	}
+	for (var i = 0; i < scanLineCount; i++) {
+		for (var j = 0; j < newEdgeTable[i].length; j++) {
+			var xPositionOfIntersection = newEdgeTable[i][j].x;
+			for (var k = i; k < newEdgeTable[i][j].maxY - minY; k++) {
+				activeEdgeTable[k].push(Math.round(xPositionOfIntersection));
+				xPositionOfIntersection += newEdgeTable[i][j].dx;
+			}
+		}
+	}
+	for (var i = 0; i < scanLineCount; i++) {
+		activeEdgeTable[i].sort(sortNumber);
+		for (var j = 0; j < activeEdgeTable[i].length; j += 2) {
+			drawLine(cxt, activeEdgeTable[i][j], activeEdgeTable[i][j + 1], i + minY, color);
+		}
+	}
+}
+
+function sortNumber(a, b) {
+	return a - b;
+}
+
+function reDraw(cxt) {
+	cxt.clearRect(0, 0, canvas.width, canvas.height);
+	for (var i = 0; i < polygon.length; i++) {
+		fillPolygon(cxt, polygon[i]);
+	}
+	drawCircle();
+}
+
+function onMouseDown() {
+	for (var i = 0; i < vertex_pos.length; i++) {
+		if (Math.abs(mouseX - vertex_pos[i][0]) < circleRadius
+				&& Math.abs(mouseY - vertex_pos[i][1]) < circleRadius) {
+			activeVertex = i;
+			break;
+		}
+	}
+}
+
+function onMouseUp() {
+	activeVertex = -1;
+}
+
+function onMouseMove(e) {
+	if (e.offsetX) {
+		mouseX = e.offsetX;
+		mouseY = e.offsetY;
+	} else if (e.layerX) {
+		mouseX = e.layerX;
+		mouseY = e.layerY;
+	}
+	if (activeVertex != -1) {
+		vertex_pos[activeVertex][0] = mouseX;
+		vertex_pos[activeVertex][1] = mouseY;
+		reDraw(cxt);
 	}
 }
 
@@ -60,21 +156,10 @@ window.onload = function() {
 	cxt = canvas.getContext("2d");
 	// 将canvas坐标整体偏移0.5，用于解决宽度为1个像素的线段的绘制问题，具体原理详见project文档
 	cxt.translate(0.5, 0.5);
-	drawCircle();
-	fillPolygon(cxt, polygon[0]);
-	// for (y = 0; y < 255; y++) {
-	// for (x = 0; x < 255; x += 1) {
-	// color = [ y % 255, x % 255, 0 ];// 这里根据坐标计算颜色，因为颜色分量必须在0~255之间，所以这里用了取余运算
-	// drawPoint(cxt, x, y, color);
-	// }
-	// }
-	// for (y = 0; y < 255; y++) {
-	// color = [ 0, 0, y % 255 ];// 这里根据坐标计算颜色，因为颜色分量必须在0~255之间，所以这里用了取余运算
-	// drawLine(cxt, 300, y, 555, y, color);
-	// }
-	//
-	// for (var x = 0, offset = 0; x < 200; x += 10, offset += 0.1) {
-	// color = [ 0, 0, 180 ];
-	// drawLine(cxt, x, 280 + offset, x + 9, 280 + offset, color);
-	// }
+	circleRadius = 10;
+	activeVertex = -1;
+	canvas.addEventListener("mousedown", onMouseDown);
+	canvas.addEventListener("mouseup", onMouseUp);
+	canvas.addEventListener("mousemove", onMouseMove);
+	reDraw(cxt);
 }
