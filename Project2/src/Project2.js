22a51c707b16823829ae7c8b748e57f7b762431f
zhouxinan <zhouxinan@protonmail.ch>
2015-11-08 17:38:08 +0800

    Implemented texture image.

--- Project2/src/Project2.js
+++ Project2/src/Project2.js
@@ -4,9 +4,12 @@ var VSHADER_SOURCE =
 	'attribute vec4 a_Color;\n' +
 	'varying vec4 v_Color;\n' +
 	'uniform mat4 u_ModelMatrix;\n' +
+	'attribute vec2 a_TexCoord;\n' +
+	'varying vec2 v_TexCoord;\n' +
 	'void main() {\n' +
 	'	gl_Position = u_ModelMatrix * a_Position;\n' +
 	'	v_Color = a_Color;\n' +
+	'	v_TexCoord = a_TexCoord;\n' +
 	'}\n';
 
 // Fragment shader program
@@ -15,9 +18,14 @@ var FSHADER_SOURCE =
 	'varying vec4 v_Color;\n' +
 	'uniform vec4 u_GridColor;\n' +
 	'uniform bool u_isToDrawGrid;\n' +
+	'uniform bool u_isToUseTextureImage;\n' +
+	'uniform sampler2D u_Sampler;\n' +
+	'varying vec2 v_TexCoord;\n' +
 	'void main() {\n' +
 	'	if (u_isToDrawGrid)\n' +
 	'		gl_FragColor = u_GridColor;\n' +
+	'	else if (u_isToUseTextureImage)\n' +
+	'		gl_FragColor = texture2D(u_Sampler, v_TexCoord);\n' +
 	'	else\n' +
 	'		gl_FragColor = v_Color;\n' +
 	'}\n';
@@ -35,6 +43,8 @@ function main() {
 	isEditModeOn = true;
 	// By default, show mode is off.
 	isShowModeOn = false;
+	// By default, texture image is not used.
+	isToUseTextureImage = false;
 	// By default, the polygons are to grow smaller.
 	isPolygonToGrowLarger = false;
 	// When the mouse is pressed within a vertex's radius, the vertex is the
@@ -91,6 +101,12 @@ function main() {
 		return;
 	}
 
+	// Set texture
+	if (!initTextures(gl, n)) {
+		console.log('Failed to intialize the texture.');
+		return;
+	}
+
 	// Specify the color for clearing <canvas>
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
@@ -103,7 +119,7 @@ function main() {
 			animate(currentAngle); // Update the rotation angle and the scale
 			draw(gl, canvas); // Draw the shape.
 			requestAnimationFrame(tick, canvas); // Request that the browser
-													// calls tick
+			// calls tick
 		}
 	}
 
@@ -171,6 +187,12 @@ function main() {
 		case 'E':
 			isShowModeOn = false;
 			isEditModeOn = true;
+			draw(gl, canvas);
+			break;
+		// If 'i' or 'I' is pressed, change the visibility of the texture image.
+		case 'i':
+		case 'I':
+			isToUseTextureImage = !isToUseTextureImage;
 			draw(gl, canvas);
 			break;
 		}
@@ -246,7 +268,7 @@ function drawPolygon(gl, canvas, polygonToDraw) {
 	}
 
 	// Set u_isToDrawGrid in the shader to 0;
-	setIsToDrawGrid(gl, false);
+	setFragmentShaderInfo(gl, false, isToUseTextureImage);
 
 	// Create a buffer object
 	var vertexBuffer = gl.createBuffer();
@@ -274,18 +296,45 @@ function drawPolygon(gl, canvas, polygonToDraw) {
 			* itemsPerVertex, 0);
 	// Enable the assignment to a_Position variable
 	gl.enableVertexAttribArray(a_Position);
+	if (isToUseTextureImage) {
+		var verticesTexCoords = new Float32Array([
+		// Texture coordinate
+		0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, ]);
+		// Create a buffer object
+		var texCoordsBuffer = gl.createBuffer();
+		if (!texCoordsBuffer) {
+			console.log('Failed to create the buffer object');
+			return;
+		}
 
-	// Get storage location of a_Color
-	var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
-	if (a_Color < 0) {
-		console.log('Failed to get the storage location of a_Color');
-		return;
+		// Bind the buffer object to target
+		gl.bindBuffer(gl.ARRAY_BUFFER, texCoordsBuffer);
+		// Write data into the buffer object
+		gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
+		// Get the storage location of a_TexCoord
+		var FSIZE = polygonVerticesColors.BYTES_PER_ELEMENT;
+		var a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
+		if (a_TexCoord < 0) {
+			console.log('Failed to get the storage location of a_TexCoord');
+			return -1;
+		}
+		// Assign the buffer object to a_TexCoord variable
+		gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 2, 0);
+		gl.enableVertexAttribArray(a_TexCoord); // Enable the assignment of the buffer object
+
+	} else {
+		// Get storage location of a_Color
+		var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+		if (a_Color < 0) {
+			console.log('Failed to get the storage location of a_Color');
+			return;
+		}
+		// Assign the buffer object to a_Color variable
+		gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE
+				* itemsPerVertex, FSIZE * 3);
+		// Enable the assignment to a_Color variable
+		gl.enableVertexAttribArray(a_Color);
 	}
-	// Assign the buffer object to a_Color variable
-	gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * itemsPerVertex,
-			FSIZE * 3);
-	// Enable the assignment to a_Color variable
-	gl.enableVertexAttribArray(a_Color);
 
 	// Draw the polygon
 	gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
@@ -305,7 +354,7 @@ function drawGrid(gl, canvas, polygonToDraw) {
 	}
 
 	// Set u_isToDrawGrid in the shader to 1;
-	setIsToDrawGrid(gl, true);
+	setFragmentShaderInfo(gl, true, isToUseTextureImage);
 
 	// Create a buffer object
 	var vertexBuffer = gl.createBuffer();
@@ -405,11 +454,19 @@ function mapCoordinate(canvas, canvasX, canvasY) {
 	};
 }
 
-function setIsToDrawGrid(gl, isToDrawGrid) {
+function setFragmentShaderInfo(gl, isToDrawGrid, isToUseTextureImage) {
 	// Get storage location of u_isToDrawGrid
 	var u_isToDrawGrid = gl.getUniformLocation(gl.program, 'u_isToDrawGrid');
 	if (u_isToDrawGrid < 0) {
 		console.log('Failed to get the storage location of u_isToDrawGrid');
+		return;
+	}
+	// Get storage location of u_isToUseTextureImage
+	var u_isToUseTextureImage = gl.getUniformLocation(gl.program,
+			'u_isToUseTextureImage');
+	if (u_isToUseTextureImage < 0) {
+		console
+				.log('Failed to get the storage location of u_isToUseTextureImage');
 		return;
 	}
 	// Set u_isToDrawGrid using isToDrawGrid.
@@ -418,4 +475,55 @@ function setIsToDrawGrid(gl, isToDrawGrid) {
 	} else {
 		gl.uniform1i(u_isToDrawGrid, 0);
 	}
+	// Set u_isToUseTextureImage using isToUseTextureImage.
+	if (isToUseTextureImage) {
+		gl.uniform1i(u_isToUseTextureImage, 1);
+	} else {
+		gl.uniform1i(u_isToUseTextureImage, 0);
+	}
+
+}
+
+function initTextures(gl, n) {
+	var texture = gl.createTexture(); // Create a texture object
+	if (!texture) {
+		console.log('Failed to create the texture object');
+		return false;
+	}
+
+	// Get the storage location of u_Sampler
+	var u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+	if (!u_Sampler) {
+		console.log('Failed to get the storage location of u_Sampler');
+		return false;
+	}
+	var image = new Image(); // Create the image object
+	if (!image) {
+		console.log('Failed to create the image object');
+		return false;
+	}
+	// Register the event handler to be called on loading an image
+	image.onload = function() {
+		loadTexture(gl, n, texture, u_Sampler, image);
+	};
+	// Tell the browser to load an image
+	image.src = 'sky.jpg';
+
+	return true;
+}
+
+function loadTexture(gl, n, texture, u_Sampler, image) {
+	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+	// Enable texture unit0
+	gl.activeTexture(gl.TEXTURE0);
+	// Bind the texture object to the target
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+
+	// Set the texture parameters
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	// Set the texture image
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+
+	// Set the texture unit 0 to the sampler
+	gl.uniform1i(u_Sampler, 0);
 }
